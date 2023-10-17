fad91708bee41cd9d4f06e77b909d068958f7635
Hear Hour <heanglyhour96@gmail.com>
2023-10-17 18:46:38 +0700

    cccs

--- connection.py
+++ connection.py
@@ -1,5 +1,23 @@
 import mysql.connector
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}".format(
+            "farmreel", "RIsnb&j!!.mH", "184.168.98.120", "3306", "farmreel"
+        )
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+
 def get_mysql():
     mydb = None
     try:
--- main.py
+++ main.py
@@ -12,11 +12,22 @@ from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
 import redis.asyncio as aioredis
 import time
+import json
+import requests
+import asyncio
 
+
+from datetime import datetime
+from sqlalchemy import Column, String, Float, Integer, Text, DateTime
+from connection import Base, SessionLocal
 app = FastAPI()
 connected_clients = set()
 __WEBSOCKETS = []
+__BAKONG_URL = "https://api-bakong.nbc.gov.kh/v1/check_transaction_by_md5"
+__BAKONG_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTg4MjI0NjAsImlhdCI6MTY5MDc4NzI2MCwiZGF0YSI6eyJpZCI6IjMyNzc5MDhjYWVhNTQzMyJ9fQ.X80yVzrIjq6L73KaDPk26WYPCCUe87YxX9GXWWixbjY'
+running = False
 
+        
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -40,8 +51,86 @@ async def startup():
 async def get_client_ip(websocket: WebSocket = Depends()):
     return 
 
+
+class Payment(Base):
+    __tablename__ = "payment"
+    __table_args__ = {'extend_existing': True} 
+    id = Column(Integer, primary_key=True)
+    hash = Column(Text)
+    md5 = Column(Text)
+    date = Column(DateTime)
+    amount = Column(Float)
+    ip = Column(String(50))
+    buykey = Column(Text)
+    
+
+def generate_key(amount):
+    match float(amount):
+        case 10.0: 
+            response = requests.get("http://139.180.147.46/farmreel/buykey?token=3991&month=1")
+            if response.status_code != 200: return None
+            return response.json()["Buykey"]
+        case 30.0: 
+            response = requests.get("http://139.180.147.46/farmreel/buykey?token=3991&month=3")
+            if response.status_code != 200: return None
+            return response.json()["Buykey"]
+        case _: return None
+    
+
+
+def verify_payment(md5: str, ip: str):
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + __BAKONG_TOKEN
+    }
+
+    payload = json.dumps({"md5": md5})
+    response = requests.request("POST", __BAKONG_URL, headers=headers, data=payload)
+    if response.status_code != 200:
+        return None
+    response = response.json()
+   
+    if "errorCode" in response:
+        if response["errorCode"] in [1, 3]:
+            return None
+    
+    hash = response["data"]["hash"]
+    _db = SessionLocal()
+    
+    try:
+        prev = _db.query(Payment).filter(Payment.hash == hash).one_or_none()
+        if prev is not None:
+            return None
+        
+        if response["data"]["toAccountId"] != "heang_lyhour@aclb":
+            return None
+        
+        amount = response["data"]["amount"]
+
+        buykey = generate_key(amount)
+        
+        if not buykey: return None
+        
+        _db.add(
+            Payment(
+                hash=hash,
+                md5=md5,
+                date=datetime.now(),
+                amount=amount,
+                ip=ip,
+                buykey=buykey
+            )
+        )
+        _db.commit()
+    except:
+        print("Error")
+    finally:
+        _db.close()
+    return buykey
+
 @app.websocket("/payment")
 async def websocket_endpoint(websocket: WebSocket, md5: str):
+
     await websocket.accept()
     client_host = websocket.headers.get("X-Forwarded-For")
     if client_host:
@@ -59,15 +148,40 @@ async def websocket_endpoint(websocket: WebSocket, md5: str):
     else:
         connected_clients.add(client_host)
 
+    md5 = str(md5).replace("\"", "")
     try:
+        text_received = False
         while True:
-            message = await websocket.receive_text()
-            print(message)
-            await websocket.send_text(message)
+            try:
+                data = await asyncio.wait_for(websocket.receive_text(), timeout=2)
+                if data == "ModalClosed":
+                    text_received = True
+                else:
+                    buykey = verify_payment(md5=md5, ip=client_host)
+                    if buykey is not None:
+                        await websocket.send_text("Buykey: {}".format(buykey))
+                        await websocket.send_text("closeModal")
+                        await websocket.close()
+                        print('MD5 :', md5 , " Buykey : ", buykey)
+                        break
+                    text_received = True
+            except asyncio.TimeoutError:
+                if not text_received:
+                    buykey = verify_payment(md5=md5, ip=client_host)
+                    if buykey is not None:
+                        await websocket.send_text("Buykey: {}".format(buykey))
+                        await websocket.send_text("closeModal")
+                        await websocket.close()
+                        print('MD5 :', md5 , " Buykey : ", buykey)
+                        break
+
     except WebSocketDisconnect:
         print("WebSocket Disconnected")
     finally:
         connected_clients.remove(client_host)
+        
+        
+#I/O
 
 app.include_router(license.router)
 
